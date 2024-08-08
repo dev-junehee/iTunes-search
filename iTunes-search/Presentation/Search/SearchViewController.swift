cc40d5548f8b0d9b2a043b45af52db559e1603dd
dev-junehee <dev.junehee@gmail.com>
2024-08-09 00:43:22 +0900

    Design: 검색 화면 UI 기본 구성 (타이틀, 서치바, 테이블뷰)

--- iTunes-search/Presentation/Search/SearchViewController.swift
+++ iTunes-search/Presentation/Search/SearchViewController.swift
@@ -6,7 +6,41 @@
 //
 
 import UIKit
+import SnapKit
 
 final class SearchViewController: BaseViewController {
+    
+    private let searchBar = {
+        let controller = UISearchController()
+        controller.searchBar.placeholder = Constant.Placeholder.search
+        return controller
+    }()
+    
+    private let tableView = {
+        let view = UITableView()
+        view.backgroundColor = .lightGray
+        return view
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func configureViewController() {
+        navigationItem.title = Constant.Title.search
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.largeTitleDisplayMode = .automatic
+        navigationItem.searchController = searchBar
+        view.addSubview(tableView)
+    }
+    
+    override func configureLayout() {
+        tableView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.horizontalEdges.bottom.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
+    
+    
     
 }
--- iTunes-search/Resource/Constant.swift
+++ iTunes-search/Resource/Constant.swift
@@ -10,5 +10,12 @@ import Foundation
 enum Constant {
     static let tab = ["검색"]
     
+    enum Title {
+        static let search = "검색"
+    }
+    
+    enum Placeholder {
+        static let search = "게임, 앱, 스토리 등을 검색해 보세요."
+    }
     
 }
